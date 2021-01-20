567c7584b8a42a022481b3f6361272b7d7e2c045
leonwanghui <wanghui71leon@gmail.com>
2021-01-20 11:45:30 +0800

    Add the first version of pybind11 support

---
+++ src/version.cc
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2021 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <include/version.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+py::str PyVersion() {
+    return py::str(mindspore::lite::Version());
+}
+
+PYBIND11_MODULE(_mindspore_lite, m) {
+    m.doc() = "MindSpore lite version";
+    m.def("version", &PyVersion, "Get MindSpore lite version function");
+}
